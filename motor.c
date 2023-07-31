27b9ebcd1935d18d2dbacd446ebddd7d2d7ed74e
F54101056 <40814498+phi622968143@users.noreply.github.com>
2023-07-31 21:19:03 +0800

    Update motor.c
    need to ask

--- motor.c
+++ motor.c
@@ -1,17 +1,41 @@
-//ControlRotation
-#include "dect_test.h"
 #include <Servo.h>
 Servo myservo;
-//int sensor=0;
+int trigPin = 11;    // 超音波感測器 Trig腳接 Arduino pin 11
+int echoPin = 12;    //超音波感測器 Echo 腳接 Arduino pin 12
+int speakerpin = 7;  //蜂鳴器 + 腳接 Arduino pin 7
+long duration, cm ;  //宣告計算距離時，需要用到的兩個實數
 
-void setup(){
+
+//int sensor=0;
+void setup() {
+  Serial.begin (9600);           //設定序列埠監控視窗 (Serial Monitor) 和 Arduino資料傳輸速率為 9600 bps (Bits Per Second)
+  pinMode(trigPin, OUTPUT);      //Arduino 對外啟動距離感測器Trig腳，射出超音波
+  pinMode(echoPin, INPUT);       //超音波被障礙物反射後，Arduino讀取感測器Echo腳的時間差
+
    myservo.attach(9);//接腳
 }
-//一次就好
-void loop(){
-   //sensor=analogRead(A0);
-   myservo.write(150);// 轉到60度
-   delay(150);
-   myservo.write(0);//轉回去
-   delay(150);
+
+void loop()
+{ //程式計算出距離值 cm
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(5);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  pinMode(echoPin, INPUT);
+  duration = pulseIn(echoPin, HIGH);
+  cm = (duration / 2) / 29.1;
+
+  Serial.println("cm");
+  Serial.print(cm);
+
+ 
+  
+
+  //sensor=analogRead(A0);
+   myservo.write(0);// 轉到60度
+   delay(6000);
+   analogWrite(9 , cm*10);
+   myservo.write(60);//轉回去
+   delay(1000);
 }
